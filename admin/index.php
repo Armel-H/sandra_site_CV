b2ad55ea859c4f277c1170fb4b56b9c6bdfbc2c9
Armel-H <sandra.herisson@lepoles.com>
2017-10-06 14:19:27 +0200

    commit n°7
    pages ajoutées

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,20 @@
+<?php 
+require('connexion.php');
+$resultat = $pdoCV -> query("SELECT * FROM t_utilisateur");
+$ligne_utilisateur = $resultat -> fetch(PDO::FETCH_ASSOC);
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>Admin : prénom nom </title>
+        <title>Admin : <?= $ligne_utilisateur['pseudo']; ?> </title>
     </head>
     <body>
-        <h1>Admin</h1>
+        <h1>Admin <?= $ligne_utilisateur['prenom']; ?></h1>
         
     </body>
 </html>
