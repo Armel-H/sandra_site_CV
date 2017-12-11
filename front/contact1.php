7c168dfc012b20abd42fcebadf2c9002266670fb
Armel-H <sandra.herisson@lepoles.com>
2017-12-11 16:57:20 +0100

    commit n°22

--- front/contact1.php
+++ front/contact1.php
@@ -4,15 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style1.css">
-  <title></title>
+  <title>Contact</title>
   <!--affichage de l'heure!-->
   <SCRIPT LANGUAGE="JavaScript">
   function HeureCheckEJS()
@@ -58,8 +56,8 @@
       <li><a href="../../experience.php">Experiences</a></li>
       <li><a href="competence1.html">Compétences</a></li>
       <li><a href="../../realisation.php">Réalisations</a></li>
-      <li><a href="loisir1.html">Loisirs</a></li>
-      <li><a href="contact1.html">Contact</a></li>
+      <li><a href="loisir.html">Loisirs</a></li>
+      <li><a href="contact1.php">Contact</a></li>
       <div class="heure">
         <br /><p class="tt" ></p><br /><br />
         <div id="ejs_heure">Initialisation</div>
@@ -71,45 +69,50 @@
   </div>
   <div class="meny-arrow"></div>
   <div class="contents">
-    <h1>Sandra HÉRISSON</h1>
     <div class="bg10">
-		<form action="#" method="post">
-			<input type="text" class="cont" id="nom" name="firstname" placeholder="Nom" required><span class="fa fa-user user"></span> <br/>
+  		<form action="#" method="post">
+  			<input type="text" class="cont" id="name" name="firstname" placeholder="Name" required><span class="fa fa-user user"></span> <br/>
 
-			<input type="text" id="email" class="cont" name="email" placeholder="Email" required>
-      <span class="fa fa-envelope-o email_icon"></span>
-      <br/>
+  			<input type="text" id="email" class="cont" name="email" placeholder="Email" required>
+        <span class="fa fa-envelope-o email_icon"></span>
+        <br/>
 
-			<input type="text" id="subjecting" class="cont" name="objet" placeholder="Objet" required> <span class="fa fa fa-pencil subject"></span> <br/>
+  			<input type="text" id="subjecting" class="cont" name="subject" placeholder="Subject" required> <span class="fa fa fa-pencil subject"></span> <br/>
 
-			<textarea rows="10" cols="40" id="boxing"  class="cont" placeholder="Message"></textarea> <span class="fa fa-comment-o comment"></span><br/>
+  			<textarea rows="10" cols="40" id="boxing"  class="cont" placeholder="Message"></textarea> <span class="fa fa-comment-o comment"></span><br/>
 
-			<input type="submit" value="Envoyer" id="submit_button">
-		</form>
-	</div>
+  			<input type="submit" value="Send a message" id="submit_button">
+  		</form>
+  	</div>
 
-    <script src="js/meny.js"></script>
-    <script>
-    var meny = Meny.create({
-      menuElement: document.querySelector( '.meny' ),
-      contentsElement: document.querySelector( '.contents' ),
-      // [optional] alignement du menu (top/right/bottom/left)
-      position: Meny.getQuery().p || 'left',
-      // [optional] hauteur du menu (pour la position top ou bottom)
-      height: 200,
-      // [optional] largeur du menu (pour la position left ou right)
-      width: 260,
-      // [optional] distance de d�clenchement du menu par rapport au menu
-      threshold: 40,
-      // [optional] utilisation des mouvement de la souris pour l'ouverture ou la fermeture
-      mouse: true,
-      // [optional] utilisation de l'approche
-      touch: true
-    });
+  </div>
+    <h1>Sandra HÉRISSON</h1>
+    <h2>Contact</h2>
+  <script src="js/meny.js"></script>
+  <script>
+  var meny = Meny.create({
+    menuElement: document.querySelector( '.meny' ),
+    contentsElement: document.querySelector( '.contents' ),
+    // [optional] alignement du menu (top/right/bottom/left)
+    position: Meny.getQuery().p || 'left',
+    // [optional] hauteur du menu (pour la position top ou bottom)
+    height: 200,
+    // [optional] largeur du menu (pour la position left ou right)
+    width: 260,
+    // [optional] distance de d�clenchement du menu par rapport au menu
+    threshold: 40,
+    // [optional] utilisation des mouvement de la souris pour l'ouverture ou la fermeture
+    mouse: true,
+    // [optional] utilisation de l'approche
+    touch: true
+  });
 
-    if( Meny.getQuery().u && Meny.getQuery().u.match( /^http/gi ) ) {
-      var contents = document.querySelector( '.contents' );
-      contents.style.padding = '0px';
-      contents.innerHTML = '<div class="cover"></div><iframe src="'+ Meny.getQuery().u +'" style="width: 100%; height: 100%; border: 0; position: absolute;"></iframe>';
-    }
-    </script>
+  if( Meny.getQuery().u && Meny.getQuery().u.match( /^http/gi ) ) {
+    var contents = document.querySelector( '.contents' );
+    contents.style.padding = '0px';
+    contents.innerHTML = '<div class="cover"></div><iframe src="'+ Meny.getQuery().u +'" style="width: 100%; height: 100%; border: 0; position: absolute;"></iframe>';
+  }
+  </script>
+
+</body>
+</html>
